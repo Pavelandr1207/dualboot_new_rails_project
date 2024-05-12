fc28a729883991997a230bc1692cf24154826893
Pavelandr1207 <surgeon1207@gmail.com>
2024-05-12 09:23:00 +0300

    add tests

---
+++ test/factories/sequence.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+    "string#{n}"
+  end
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+  sequence :avatar do |n|
+    "#{n}.png"
+  end
+end
+
