04d8071c33c09740a069c1cb2fb37d3bdcab1934
Pavelandr1207 <surgeon1207@gmail.com>
2024-05-14 21:32:09 +0300

    add associassions and validations

--- test/models/manager_test.rb
+++ test/models/manager_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class ManagerTest < ActiveSupport::TestCase
    test "create" do
      manager = create :manager
-    assert manager.persisted?
+     assert manager.persisted?
   end
 end
